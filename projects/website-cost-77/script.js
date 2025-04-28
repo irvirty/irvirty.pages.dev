b20670271ac80864dd5e70527fe7d1891153053a
irvirty <irvirty@gmail.com>
2025-04-28 08:17:53 +0300

    Update

--- projects/website-cost-77/script.js
+++ projects/website-cost-77/script.js
@@ -91,7 +91,8 @@ document.getElementById("result").innerHTML = print;
 
 //https://stackoverflow.com/questions/2901102/how-to-format-a-number-with-commas-as-thousands-separators
 function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+//return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
 
 
