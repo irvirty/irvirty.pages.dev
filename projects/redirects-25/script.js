61e707bac88a352ca48274f0dc8c9c579db87a4e
irvirty <irvirty@gmail.com>
2025-08-29 17:50:58 +0300

    Update

--- projects/redirects-25/script.js
+++ projects/redirects-25/script.js
@@ -932,6 +932,25 @@ sRedirectUrl = url;
 break;
 
 
+case 'yy#':
+case '..#':
+q = q3.replace(qCom, '');
+q = q.trim();
+q = encodeURIComponent(q);
+urlList = [
+"https://www.youtube.com/results?search_query=" + q + "&sp=EgQQARgC",
+];
+if (q == ''){
+urlList = [
+"https://www.youtube.com/",
+];
+}
+random = urlList[fuMRandom(0, urlList.length - 1)];
+url = random;
+sRedirectUrl = url;
+break;
+
+
 case 'ch#':
 q = q3.replace(qCom, '');
 q = q.trim();
