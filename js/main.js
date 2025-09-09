98e3befb526981ad6be9453787546085bfabec18
irvirty <irvirty@gmail.com>
2025-09-09 20:43:17 +0300

    Update

--- js/main.js
+++ js/main.js
@@ -535,9 +535,9 @@ document.getElementById("footer").innerHTML = `
 <a id="fSettings" class="brand inlineBlock padding2" title="Settings" href="${confD}pages/settings/">Settings</a>
 <a id="fPrivacy" class="brand inlineBlock padding" title="Cookie Settings" href="${confD}pages/settings/#confDataCollection">Cookie: ${conf["confDataCollection"]}</a>
 <a class="brand inlineBlock padding" title="Source code (repository)" href="https://github.com/${conf["confUsername"]}/${conf["confWebsiteUrl"]}">Source Code</a>
-<span class="op inlineBlock padding gray"><!--2019--->2025</span>
 <span class="gray">License:</span>
-<a class="brand inlineBlock padding" rel="license" title="Main license" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a><a class="brand inlineBlock padding" title="Other license" href="${confD}pages/about/#license"><sup class="xSmall">Other license</sup></a>
+<a class="brand inlineBlock padding" rel="license" title="Main license" href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA 4.0</a><a class="brand inlineBlock padding" title="Other license" href="${confD}pages/about/#license"><sup class="xSmall">* Other</sup></a>
+<span class="op inlineBlock padding gray" title="update"><!--2019--->2025</span>
 <br>
 <span class="gray">Hosting:</span>
 <a class="brand inlineBlock padding" style="padding-right: 0;" title="Hosting Service" href="https://pages.cloudflare.com/">Cloudflare Pages</a>
