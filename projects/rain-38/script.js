67fd4610a535aa79d74e84cc4467086e1e2924cf
irvirty <irvirty@gmail.com>
2025-09-24 10:59:09 +0300

    update

--- projects/rain-38/script.js
+++ projects/rain-38/script.js
@@ -34,13 +34,29 @@ print =
 
 document.getElementById("snowPrint").innerHTML = print;
 
-if (conf["confDataCollection"] == "on"||conf["confDataCollection"] == "allow embed"){
+//if (conf["confDataCollection"] == "on"||conf["confDataCollection"] == "allow embed"){}
 	if (document.getElementById("printAudio") != null){
 		document.getElementById("printAudio").innerHTML = `
 
-<audio controls width="250" autoplay="autoplay">
+<!--<audio controls width="250" autoplay="autoplay">
   <source src="https://upload.wikimedia.org/wikipedia/commons/9/99/Mountain_Stream_Rain_Shower_ASMR.webm" type="video/webm">
 </audio>
+
+<br>
+
+<a class="brand button" target="blank" href="https://commons.wikimedia.org/wiki/File:Mountain_Stream_Rain_Shower_ASMR.webm">File:Mountain Stream Rain Shower ASMR.webm - Wikimedia Commons</a><br>
+
+License: <a href="https://commons.wikimedia.org/wiki/File:Mountain_Stream_Rain_Shower_ASMR.webm">Jarrod stanley</a>, CC0, via Wikimedia Commons<br>
+
+-->
+
+<!--https://developer.mozilla.org/en-US/docs/Web/HTML/Reference/Elements/audio#-->
+<figure>
+  <figcaption>rain.mp3:</figcaption>
+  <audio controls autoplay loop src="${confD}audio/rain.mp3"></audio>
+<br>
+  <a href="${confD}audio/rain.mp3"> Download audio </a>
+</figure>
 		`;
 	}
-}
+
