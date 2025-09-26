bf9700783c439cda0f36ead8e55393dadddc3035
irvirty <irvirty@gmail.com>
2025-09-26 04:04:14 +0300

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-//var myCacheVersion = "v.1.2.2025.09.25";
+//var myCacheVersion = "v.1.2.2025.09.26";
 
 
 
@@ -70,7 +70,7 @@ function swJsInstallFiles(){
 //https://stackoverflow.com/questions/66529102/uncaught-in-promise-typeerror-failed-to-execute-cache-on-addall-request
 //https://github.com/mdn/pwa-examples
 self.addEventListener('install', (e) => {
-e.waitUntil(caches.open("v.1.2.2025.09.25").then((cache) => cache.addAll(fileListArrFound)),);
+e.waitUntil(caches.open("v.1.2.2025.09.26").then((cache) => cache.addAll(fileListArrFound)),);
 });*/
 
 
@@ -1473,7 +1473,7 @@ if (res.ok) {
 // file is present at URL
 console.log('try cache.add ' + file);
 //https://web.dev/learn/pwa/caching/
-caches.open("v.1.2.2025.09.25")
+caches.open("v.1.2.2025.09.26")
 .then(cache => {
 cache.add(file); // it stores only one resource
 //  cache.addAll(["styles.css", "app.js"]); // it stores two resources
@@ -1499,7 +1499,7 @@ if (res.ok) {
 // file is present at URL
 console.log('try cache.add ' + newFile);
 //https://web.dev/learn/pwa/caching/
-caches.open("v.1.2.2025.09.25")
+caches.open("v.1.2.2025.09.26")
 .then(cache => {
 cache.add(newFile); // it stores only one resource
 //  cache.addAll(["styles.css", "app.js"]); // it stores two resources
@@ -1532,7 +1532,7 @@ console.log('404 not found ' + newFile);
 // rm old cache
 //https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/delete
 this.addEventListener("activate", (event) => {
-  const cachesToKeep = ["v.1.2.2025.09.25"];
+  const cachesToKeep = ["v.1.2.2025.09.26"];
 
   event.waitUntil(
     caches.keys().then((keyList) =>
