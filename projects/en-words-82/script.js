9a182220911e4f92c42063ff8138fd932b7ee8c5
irvirty <irvirty@gmail.com>
2025-12-30 07:30:10 +0200

    update

--- projects/en-words-82/script.js
+++ projects/en-words-82/script.js
@@ -1,4 +1,4 @@
-// v.1.0.0
+// v.1.1.0
 
 
 let print = "";
@@ -11,11 +11,18 @@ print += `${val["text"]}<br>`;
 
 }
 
+//https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
+var randomItem = enWordsJsonVar[Math.floor(Math.random() * enWordsJsonVar.length)];
+randomItem = randomItem["text"];
 
 document.getElementById("result").innerHTML = `
 total: ${enWordsJsonVar.length}
+random: ${randomItem}
+
 ${print}
 total: ${enWordsJsonVar.length}
+random: ${randomItem}
+
 `;
 
 
