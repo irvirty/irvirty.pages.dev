9dc4a8a24a7a3de6c33863660ed239130a1b4f84
irvirty <irvirty@gmail.com>
2026-03-24 19:08:43 +0200

    update

--- data/quizJsonVar.js
+++ data/quizJsonVar.js
@@ -6,9 +6,9 @@ var quizJsonVar =
         "id": "13",
         "text": "Inspired: meme.",
         "text2": "Which example JS code will result in \"false\"?",
-        "text3": "0\/0 == NaN|||true|||\r\n___\r\n\"0 == \"\"|||false|||\r\n___\r\nMath.min() > Math.max()\"|||false|||\r\n___\r\n0\/1 == NaN|||true|||\r\n___\r\nNaN === NaN|||true|||\r\n___\r\ntypeof null === 'object|||false|||\r\n___\r\n1 == 1|||false|||\r\n___",
+        "text3": "0\/0 == NaN|||true|||\r\n___\r\n\"0 == \"\"|||false|||\r\n___\r\nMath.min() > Math.max()\"|||false|||\r\n___\r\n0\/1 == NaN|||true|||\r\n___\r\nNaN === NaN|||true|||\r\n___\r\ntypeof null === 'object'|||false|||\r\n___\r\n1 == 1|||false|||\r\n___",
         "url": "",
-        "tag": "#code #JavaScript",
+        "tag": "#all #code #JavaScript",
         "time": 1725069118,
         "rightFooter": ""
     },
@@ -18,7 +18,7 @@ var quizJsonVar =
         "text2": "Which of these is an HTML tag?",
         "text3": "<head>|||true|||\r\n___\r\n<title>|||true|||\r\n___\r\n<body>|||true|||\r\n___\r\n<header>|||true|||\r\n___\r\n<footer>|||true|||\r\n___\r\n<article>|||true|||\r\n___\r\n<section>|||true|||\r\n___\r\n<p>|||true|||\r\n___\r\n<div>|||true|||\r\n___\r\n<span>|||true|||\r\n___\r\n<img>|||true|||\r\n___\r\n<audio>|||true|||\r\n___\r\n<nav>|||true|||\r\n___\r\n<progress>|||true|||\r\n___\r\n<video>|||true|||\r\n___\r\n<ul>|||true|||\r\n___\r\n<ol>|||true|||\r\n___\r\n<li>|||true|||\r\n___\r\n<ol>|||true|||\r\n___\r\n\r\n<div>|||true|||\r\n___\r\n<span>|||true|||\r\n___\r\n<em>|||true|||\r\n___\r\n<b>|||true|||\r\n___\r\n<h1>|||true|||\r\n___\r\n\r\n<hamburger>|||false|||\r\n___\r\n<bento>|||false|||\r\n___\r\n<line>|||false|||\r\n___\r\n<submenu>|||false|||\r\n___\r\n<navigation>|||false|||\r\n___",
         "url": "https:\/\/developer.mozilla.org\/en-US\/docs\/Web\/HTML",
-        "tag": "#code #HTML",
+        "tag": "#all #code #HTML",
         "time": 1705334821,
         "rightFooter": ""
     },
@@ -28,7 +28,7 @@ var quizJsonVar =
         "text2": "What HTML element produces a line break in text (carriage-return)?",
         "text3": "<br>|||true|||\r\n___\r\n<div>|||false|||\r\n___\r\n<break>|||false|||\r\n___\r\n<span>|||false|||\r\n___\r\n<em>|||false|||\r\n___\r\n<b>|||false|||\r\n___\r\n<h1>|||false|||\r\n___\r\n<ul>|||false|||\r\n___",
         "url": "https:\/\/developer.mozilla.org\/en-US\/docs\/Web\/HTML\/Element\/br",
-        "tag": "#code #HTML",
+        "tag": "#all #code #HTML",
         "time": 1705329648,
         "rightFooter": ""
     },
@@ -38,7 +38,7 @@ var quizJsonVar =
         "text2": "The eighth and farthest planet from the Sun. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet.",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||true|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Neptune",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1701886018,
         "rightFooter": ""
     },
@@ -48,7 +48,7 @@ var quizJsonVar =
         "text2": "The seventh planet from the Sun. It is a gaseous cyan-coloured ice giant.",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||true|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Uranus",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1701885858,
         "rightFooter": ""
     },
@@ -58,7 +58,7 @@ var quizJsonVar =
         "text2": "The sixth planet from the Sun and the second-largest in the Solar System.",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||true|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Saturn",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1701885694,
         "rightFooter": ""
     },
@@ -68,7 +68,7 @@ var quizJsonVar =
         "text2": "The fourth planet and the furthest terrestrial planet from the Sun. The reddish color of its surface is due to finely grained iron(III) oxide dust in the soil, giving it the nickname \"the Red Planet\".",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||true|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Mars",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1701885565,
         "rightFooter": ""
     },
@@ -78,7 +78,7 @@ var quizJsonVar =
         "text2": "The third planet from the Sun and the only astronomical object known to harbor life.",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||true|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Earth",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1701885256,
         "rightFooter": ""
     },
@@ -88,7 +88,7 @@ var quizJsonVar =
         "text2": "The second planet from the Sun. It is a rocky planet with the densest atmosphere of all the rocky bodies in the Solar System, and the only one with a mass and size that is close to that of its orbital neighbour Earth.",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||true|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Venus",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1701885136,
         "rightFooter": ""
     },
@@ -98,7 +98,7 @@ var quizJsonVar =
         "text2": "What will this JS code output?\r\nconsole.log(\"Hello, World!\");",
         "text3": "Hello, World!|||true|||\r\n___\r\n\"Hello, World!\"|||false|||\r\n___\r\nHello,|||false|||\r\n___\r\nWorld!|||false|||\r\n___\r\n(\"Hello, World!\")|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/JavaScript#Simple_examples",
-        "tag": "#code #JavaScript",
+        "tag": "#all #code #JavaScript",
         "time": 1699848590,
         "rightFooter": ""
     },
@@ -108,7 +108,7 @@ var quizJsonVar =
         "text2": "The smallest planet in the Solar System",
         "text3": "Mercury|||true|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||false|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Mercury_(planet)",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1698655981,
         "rightFooter": ""
     },
@@ -118,7 +118,7 @@ var quizJsonVar =
         "text2": "A planet of the solar system.",
         "text3": "Mercury|||true|||\r\n___\r\nVenus|||true|||\r\n___\r\nEarth|||true|||\r\n___\r\nMars|||true|||\r\n___\r\nJupiter|||true|||\r\n___\r\nSaturn|||true|||\r\n___\r\nUranus|||true|||\r\n___\r\nNeptune|||true|||\r\n___\r\nArrakis|||false|||\r\n___\r\nCoruscant|||false|||\r\n___\r\nKrypton|||false|||\r\n___\r\nQward|||false|||\r\n___\r\nSkaro|||false|||\r\n___\r\nXandar|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Solar_System",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1698655327,
         "rightFooter": ""
     },
@@ -128,7 +128,7 @@ var quizJsonVar =
         "text2": "Fifth planet from the Sun and the largest in the Solar System.",
         "text3": "Mercury|||false|||\r\n___\r\nVenus|||false|||\r\n___\r\nEarth|||false|||\r\n___\r\nMars|||false|||\r\n___\r\nJupiter|||true|||\r\n___\r\nSaturn|||false|||\r\n___\r\nUranus|||false|||\r\n___\r\nNeptune|||false|||\r\n___",
         "url": "https:\/\/en.wikipedia.org\/wiki\/Jupiter",
-        "tag": "#science #space",
+        "tag": "#all #science #space",
         "time": 1698648497,
         "rightFooter": ""
     }
