b72dd10fa457db67b63dcbac871feccaf1bbc7e4
irvirty <irvirty@gmail.com>
2026-05-02 08:07:40 +0300

    update

--- projects/en-words-82/script.js
+++ projects/en-words-82/script.js
@@ -22,19 +22,25 @@ counter++;
 let averageWordLength = (average / counter).toFixed(2);
 
 //https://stackoverflow.com/questions/5915096/get-a-random-item-from-a-javascript-array
-var randomItem = enWordsJsonVar[Math.floor(Math.random() * enWordsJsonVar.length)];
-randomItem = randomItem["text"];
+var randomItem1 = enWordsJsonVar[Math.floor(Math.random() * enWordsJsonVar.length)];
+randomItem1 = randomItem1["text"];
+
+var randomItem2 = enWordsJsonVar[Math.floor(Math.random() * enWordsJsonVar.length)];
+randomItem2 = randomItem2["text"];
+
+var randomItem3 = enWordsJsonVar[Math.floor(Math.random() * enWordsJsonVar.length)];
+randomItem3 = randomItem3["text"];
 
 document.getElementById("result").innerHTML = `
 Total: ${enWordsJsonVar.length}
-Random: ${randomItem}
+Random: ${randomItem1}, ${randomItem2}, ${randomItem3}
 Average word length: ${averageWordLength}
 Average word length from Google: 5.1
 
 ${print}
 
 Total: ${enWordsJsonVar.length}
-Random: ${randomItem}
+Random: ${randomItem1}, ${randomItem2}, ${randomItem3}
 Average word length: ${averageWordLength}
 Average word length from Google: 5.1
 
