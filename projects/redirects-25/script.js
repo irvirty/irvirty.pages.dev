1c36b274d35b2c05e47accc7b962b01f25bd3534
irvirty <irvirty@gmail.com>
2026-07-04 05:56:54 +0300

    update

--- projects/redirects-25/script.js
+++ projects/redirects-25/script.js
@@ -613,11 +613,11 @@ q = q3.replace(qCom, '');
 q = q.trim();
 q = encodeURIComponent(q);
 urlList = [
-"https://social.netdynamics.eu/search?q=" + q,
+"https://nostter.app/search?q=" + q + "&proxy=on",
 ];
 if (q == ""){
 urlList = [
-"https://social.netdynamics.eu/",
+"https://nostter.app/",
 ];
 }
 random = urlList[fuMRandom(0, urlList.length - 1)];
